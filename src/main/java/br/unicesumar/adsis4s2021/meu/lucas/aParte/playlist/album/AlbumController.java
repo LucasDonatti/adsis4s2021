7f49a78e9a131cad16254743cd83b11845313be1
LucasDonatti <lucas.frantti@gmail.com>
2021-09-29 07:56:22 -0300

    Add AlbumDTO

--- src/main/java/br/unicesumar/adsis4s2021/meu/lucas/aParte/playlist/album/AlbumController.java
+++ src/main/java/br/unicesumar/adsis4s2021/meu/lucas/aParte/playlist/album/AlbumController.java
@@ -1,5 +1,8 @@
 package br.unicesumar.adsis4s2021.meu.lucas.aParte.playlist.album;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +11,10 @@ import br.unicesumar.adsis4s2021.meu.lucas.aParte.playlist.base.PlaylistBaseCont
 @RestController
 @RequestMapping("albuns")
 public class AlbumController extends PlaylistBaseController<Album, AlbumRepository> {
+	
+	@GetMapping("/dto")
+	public List<AlbumDTO> getAlbumDTO() {
+		return repo.encontrarAlbumDTO();
+	}
 	
 }
--- src/main/java/br/unicesumar/adsis4s2021/meu/lucas/aParte/playlist/album/AlbumRepository.java
+++ src/main/java/br/unicesumar/adsis4s2021/meu/lucas/aParte/playlist/album/AlbumRepository.java
@@ -1,7 +1,14 @@
 package br.unicesumar.adsis4s2021.meu.lucas.aParte.playlist.album;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface AlbumRepository extends JpaRepository<Album, String> {
 
+	@Query(value = "select new br.unicesumar.adsis4s2021.meu.lucas.aParte.playlist.album.AlbumDTO("
+			+ "id, nome, quantidadeMusicas, duracao, lancamento) from Album")
+	List<AlbumDTO> encontrarAlbumDTO();
+	
 }
