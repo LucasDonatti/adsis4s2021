73b3da83ef12c6cd1aa19bf842b9687eb1fb14ab
LucasDonatti <lucas.frantti@gmail.com>
2021-10-10 16:05:52 -0300

    Add MeuExceptionManager (ControllerAdvice e ExceptionHandler)

---
+++ src/main/java/br/unicesumar/adsis4s2021/meu/lucas/base/MeuBadRequestException.java
@@ -0,0 +1,9 @@
+package br.unicesumar.adsis4s2021.meu.lucas.base;
+
+public class MeuBadRequestException extends RuntimeException {
+
+	public MeuBadRequestException(String message) {
+		super(message);
+	}
+
+}
--- src/main/java/br/unicesumar/adsis4s2021/meu/lucas/base/MeuBaseController.java
+++ src/main/java/br/unicesumar/adsis4s2021/meu/lucas/base/MeuBaseController.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,12 +28,14 @@ public class MeuBaseController<ENTITY extends MeuBaseEntity, REPO extends JpaRep
 	}
 	
 	@PostMapping
-	public String post(@RequestBody ENTITY novo) {
-		if (repo.findById(novo.getId()).isPresent()) {
-			throw new RuntimeException("Seu registro já existe, faça um put ao invés de post!");
+	//@ResponseStatus(value = HttpStatus.CREATED)
+	public ResponseEntity<String> post(@RequestBody ENTITY novo) {
+		if (novo.getId() != null && repo.findById(novo.getId()).isPresent()) {
+			//throw new RuntimeException("Seu registro já existe, faça um put ao invés de post!");
+			return ResponseEntity.status(HttpStatus.CONFLICT).body("Seu registro já existe, faça um put ao invés de post!");
 		}
 		novo = repo.save(novo);
-		return novo.getId();
+		return ResponseEntity.status(HttpStatus.CREATED).body(novo.getId());
 	}
 	
 //	@PostMapping
